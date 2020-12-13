e28b752ccb290822d044b18fac62605755fbe23e
Eric <elepki@hotmail.com>
2020-12-13 00:59:24 -0600

    Added email and password sign-in page and page styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import Header from "./components/header/header.component";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/sign" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
