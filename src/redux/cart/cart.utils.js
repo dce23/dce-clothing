256a1c4352a3f6eaa81e14b4a2d7a64a1337b14c
Eric <elepki@hotmail.com>
2021-01-05 15:13:17 -0600

    Updated shop data with new data, updated cart utils to group items in cart reducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,20 @@
+/* Utility functions allow you to keep the files clean and organize
+   functions that i may need in multiple files in one location
+*/
+
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  // quantity property gets attached the first time around since the if block wont run when its a new item
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
